89e31a41736397bb57c06fb566cc5b49ccf2e0a0
xiaochun <xchun90@163.com>
2024-05-28 17:23:47 +0800

    Try optimize style

--- src/main/java/com/zrlog/plugin/backup/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/backup/GraalvmAgentApplication.java
@@ -9,7 +9,6 @@ import com.zrlog.plugin.render.FreeMarkerRenderHandler;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +25,7 @@ public class GraalvmAgentApplication {
         File file = new File(basePath + "/src/main/resources");
         PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath() + "/", "/");
         //Application.nativeAgent = true;
+        new FreeMarkerRenderHandler().render("/template/index", new Plugin(), new HashMap<>());
         PluginNativeImageUtils.exposeController(Collections.singletonList(BackupController.class));
         Application.main(args);
 
