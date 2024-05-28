7c88f13490024be62a23b5e50546cbc1f3ead617
xiaochun <xchun90@163.com>
2024-05-28 15:47:48 +0800

    Try optimize style

--- bin/package-native-github.sh
+++ bin/package-native-github.sh
@@ -4,4 +4,4 @@ java -version
 ./mvnw -Pnative package
 basePath=/tmp/download/plugin
 mkdir -p ${basePath}
-mv target/oss ${basePath}/oss-$(uname -s)-$(uname -m).bin
+mv target/buckup-sql-file ${basePath}/buckup-sql-file-$(uname -s)-$(uname -m).bin
