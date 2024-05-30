888f97819d161f08c51ce4ccb4cabe5459b12c79
xiaochun <xchun90@163.com>
2024-05-30 16:32:40 +0800

    Try optimize style

--- src/main/java/com/zrlog/plugin/backup/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/backup/GraalvmAgentApplication.java
@@ -1,10 +1,7 @@
 package com.zrlog.plugin.backup;
 
-import com.google.gson.Gson;
 import com.zrlog.plugin.backup.controller.BackupController;
 import com.zrlog.plugin.common.PluginNativeImageUtils;
-import com.zrlog.plugin.data.codec.FileDesc;
-import com.zrlog.plugin.data.codec.HttpRequestInfo;
 import com.zrlog.plugin.message.Plugin;
 import com.zrlog.plugin.render.FreeMarkerRenderHandler;
 
@@ -18,12 +15,7 @@ public class GraalvmAgentApplication {
 
 
     public static void main(String[] args) throws IOException {
-        new Gson().toJson(new HttpRequestInfo());
-        new Gson().toJson(new FileDesc());
-        new Gson().toJson(new HttpRequestInfo());
-        new Gson().toJson(new Plugin());
-        new Gson().toJson(new HashMap<>());
-        new Gson().fromJson("{}", Map.class);
+        PluginNativeImageUtils.usedGsonObject();
         String basePath = System.getProperty("user.dir").replace("/target", "");
         File file = new File(basePath + "/src/main/resources");
         PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath() + "/", "/");
@@ -34,6 +26,7 @@ public class GraalvmAgentApplication {
         plugin.setVersion("test");
         Map<String, Object> objectObjectHashMap = new HashMap<>();
         objectObjectHashMap.put("cycle", "3600");
+        objectObjectHashMap.put("theme", "light");
         new FreeMarkerRenderHandler().render("/templates/index.ftl", plugin, objectObjectHashMap);
         PluginNativeImageUtils.exposeController(Collections.singletonList(BackupController.class));
         Application.main(args);
