bc526fe5854b5829a96226173f9ea98f3c9967bb
xiaochun <xchun90@163.com>
2024-06-01 15:42:38 +0800

    Try optimize style

--- bin/package-native-github.sh
+++ bin/package-native-github.sh
@@ -1,7 +1,17 @@
+#!/usr/bin/env bash
+
 java -version
 ./mvnw clean package
 ./mvnw -Pnative -Dagent exec:exec@java-agent -U
 ./mvnw -Pnative package
 basePath=/tmp/download/plugin
 mkdir -p ${basePath}
-mv target/backup-sql-file ${basePath}/backup-sql-file-$(uname -s)-$(uname -m).bin
+binName=backup-sql-file
+if [ -f "target/${binName}" ];
+then
+  mv target/${binName} ${basePath}/plugin-core-$(uname -s)-$(uname -m).bin
+fi
+if [ -f "target/${binName}.exe" ];
+then
+  mv target/${binName}.exe ${basePath}/${binName}-Windows-$(uname -m).bin
+fi
